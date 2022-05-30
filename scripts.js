21859a8233e501ccb7087457b93958f2c841917b
Matt <blooom@gmail.com>
2022-05-30 18:25:02 -0400

    addBookToLibrary() has been added and is functional

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-
   // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
-
-  this.read = function () {
-      if(this.read === true){
-          return "has been read";
-      } else {
-          return "not read yet";
-      }
-  }
+  this.read = read;
 
   this.info = function () {
-      return (`${title} by ${author}, ${pages} pages, ${read}`)
-  }
-
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+  };
 }
 
 function addBookToLibrary() {
   // do stuff here
+  let title = prompt("Enter title of book");
+  let author = prompt("Enter author of book");
+  let pages = prompt("Enter number of pages");
+  let read = "";
+  let readPrompt = prompt("Has this book been read?").toLowerCase();
+  if (readPrompt == "yes") {
+    read = "has been read.";
+  } else if ((readPrompt = "no")) {
+    read = "not yet read.";
+  }
+
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
+
+addBookToLibrary();
+console.log(myLibrary[0].info());
